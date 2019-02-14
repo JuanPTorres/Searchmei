0daffde172a70d5814ac9bca195ff30d08338f88
JuanPTorres <47615548+JuanPTorres@users.noreply.github.com>
2019-02-13 20:51:54 -0500

    Login
    Creacion de Login en un pequeño formulario

---
+++ application/views/view_login.php
@@ -0,0 +1,28 @@
+<?php
+echo base_url('');
+?> 
+<html>
+<body>
+
+<form action="index.html" align="center">
+  <div class="imagen">
+    <img src="images/descarga.png">
+  </div>
+  <br><br><br><br><br>
+  <div class="Login"><font face="Arial">
+    <label for="uname"><b>Usuario&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></label>
+    <input type="text" name="user">
+    <br><br>
+    <label for="psw"><b>Contraseña </b></label>
+    <input type="password" name="password">
+    <br><br><br>
+    <button type="submit">Login</button>
+  	</font>
+  </div>
+
+
+</form>
+
+
+</body>
+</html>
